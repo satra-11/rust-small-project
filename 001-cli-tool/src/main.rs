87230d465f510e7db400f97f1e6505cb43eb7611
satra-11 <syuzuki546@gmail.com>
2026-01-05 12:40:39 +0900

    feat: add basic CLI argument parsing for pattern and path, replacing initial greeting.

--- 001-cli-tool/src/main.rs
+++ 001-cli-tool/src/main.rs
@@ -1,3 +1,16 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("No pattern given");
+    let path = std::env::args().nth(2).expect("No path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path)
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
