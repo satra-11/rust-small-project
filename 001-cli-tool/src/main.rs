8b855e29858111aa9d5f16103b9bcd46b71e3f2d
satra-11 <syuzuki546@gmail.com>
2026-01-06 20:20:09 +0900

    feat: Introduce `clap` crate for robust command-line argument parsing.

--- 001-cli-tool/src/main.rs
+++ 001-cli-tool/src/main.rs
@@ -1,16 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path)
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
