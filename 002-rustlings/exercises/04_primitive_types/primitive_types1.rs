8b78c905855d89f95013ef79c4ba6fa91c8a0c56
satra-11 <syuzuki546@gmail.com>
2026-01-13 13:50:38 +0900

    update: rustlings

--- 002-rustlings/exercises/04_primitive_types/primitive_types1.rs
+++ 002-rustlings/exercises/04_primitive_types/primitive_types1.rs
@@ -9,7 +9,23 @@ fn main() {
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
     // let …
+    let is_evening = false;
     if is_evening {
         println!("Good evening!");
     }
 }
+
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+}
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+    }
+}
+
