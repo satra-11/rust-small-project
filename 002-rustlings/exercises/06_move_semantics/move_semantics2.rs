6a94ec1b3276db4a13b2c572996dd4efdfd905b9
satra-11 <syuzuki546@gmail.com>
2026-01-15 17:32:00 +0900

    chore: update rustlings

--- 002-rustlings/exercises/06_move_semantics/move_semantics2.rs
+++ 002-rustlings/exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,9 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+    let vec1 : &mut Vec<i32> = vec;
 
-    vec.push(88);
+    vec1.push(88);
 
-    vec
+    vec1.to_vec()
 }
 
 fn main() {
@@ -20,7 +20,7 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&mut vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
